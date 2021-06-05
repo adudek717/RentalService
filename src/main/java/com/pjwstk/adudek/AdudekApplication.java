55e5f104f2611dc39bf8f150de31c6d995d67559
adudek717 <a.dudek717@gmail.com>
2021-06-05 16:36:43 +0200

    first commit

---
+++ src/main/java/com/pjwstk/adudek/AdudekApplication.java
@@ -0,0 +1,13 @@
+package com.pjwstk.adudek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdudekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdudekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pjwstk/adudek/AdudekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjwstk.adudek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdudekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
