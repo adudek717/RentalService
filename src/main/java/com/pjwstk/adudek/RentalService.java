9c4862c2d63c445306fc2e1ec381873fea8d0166
adudek717 <a.dudek717@gmail.com>
2021-06-05 18:40:05 +0200

    small refactoring, code 2.0

--- src/main/java/com/pjwstk/adudek/RentalService.java
+++ src/main/java/com/pjwstk/adudek/RentalService.java
@@ -1,19 +1,26 @@
 package com.pjwstk.adudek;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class RentalService {
     private final RestTemplate restTemplate;
-    public RentalService(RestTemplate restTemplate) { this.restTemplate = restTemplate; }
-    String movieServiceAddress = "http://localhost:8080/movies/";
+    private final String movieServiceAddress;
+
+    public RentalService(RestTemplate restTemplate, @Value("${service.movie.address}") String movieServiceAddress) {
+        this.restTemplate = restTemplate;
+        this.movieServiceAddress = movieServiceAddress;
+    }
 
     public Movie getMovie(Long id) {
-        return restTemplate.getForObject(movieServiceAddress + id, Movie.class);
+        String url = movieServiceAddress + "/movies/" + id;
+        return restTemplate.getForObject(url, Movie.class);
     }
 
     public void returnMovie(Long id) {
-        restTemplate.put(movieServiceAddress + "isAvailable/" + id, null);
+        String url = movieServiceAddress + "/movies/isAvailable/" + id;
+        restTemplate.put(url, null);
     }
 }
